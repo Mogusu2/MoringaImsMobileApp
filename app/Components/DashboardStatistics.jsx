a04fb9104aaeea02569a51874b55f128770141d3
Hosea <karanja.mungai23@gmail.com>
2025-02-20 11:57:48 +0300

    Updates dashboard

---
+++ app/Components/DashboardStatistics.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import colors from "../config/colors";
+function DashboardStatistics({icon, title, value}) {
+    return (
+        <View style={styles.container}>
+            <Icon name={icon} style={{marginTop: 5}} size={50} color={colors.darkerShadeOfWhite} />
+            <Text style={{color: colors.darkerShadeOfWhite, fontSize: 12, fontWeight: '900'}} >{value}</Text>
+            <Text style={{color: colors.darkerShadeOfWhite, paddingBottom: 7 ,fontSize: 15, fontWeight: '900'}} >{title}</Text>
+            
+        </View>
+    );
+}   
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.blue,
+        width: '32%',
+        alignItems: 'center',
+        borderRadius: 10,
+        
+    }
+})
+export default DashboardStatistics
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,7 +1,8 @@
 export default {
     blue:'#07013A',
-    orage: '#FC4F11',
+    orange: '#FC4F11',
     white: '#FFFFFF',
     black: '#000000',
     grey: '#707070',
+    darkerShadeOfWhite: 'rgb(206, 191, 191)'
 }
